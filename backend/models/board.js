a5659666864b2e5a572220f7d7cb064d50a8169f
nirakar007 <neerakarsuwal106@gmail.com>
2025-03-02 02:31:03 +0545

    crucial fix to image upload

--- backend/models/board.js
+++ backend/models/board.js
@@ -4,11 +4,11 @@ const elementSchema = new mongoose.Schema({
   type: {
     type: String,
     required: true,
-    enum: ["text", "image", "sticky", "doodle"], // Add more types as needed
+    enum: ["text", "image"],
   },
   content: {
     type: String, // For text, image URLs, doodle paths, etc.
-    required: true,
+    required: false,
   },
   position: {
     x: { type: Number, default: 0 }, // X-coordinate on the canvas
@@ -18,18 +18,25 @@ const elementSchema = new mongoose.Schema({
     width: { type: Number, default: 100 }, // Width of the element
     height: { type: Number, default: 100 }, // Height of the element
   },
+  image: {
+    type: String,
+    required: function () {
+      {
+        return this.type === "file";
+      }
+    },
+  },
   createdAt: {
     type: Date,
     default: Date.now,
   },
-  
 });
 
 const boardSchema = new mongoose.Schema({
   boardName: { type: String, required: true, trim: true },
   elements: [elementSchema],
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Add this
-  createdAt: { type: Date, default: Date.now }, 
+  createdAt: { type: Date, default: Date.now },
   isFavorite: {
     type: Boolean,
     default: false,
